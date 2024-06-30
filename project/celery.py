9bfbdf59ba518669ecabaa41d3189baef7f16319
Anastasia Shvechkova <94508940+Shvechkova@users.noreply.github.com>
2024-06-30 19:22:27 +0400

    Update celery.py

--- project/celery.py
+++ project/celery.py
@@ -16,20 +16,20 @@ app.conf.beat_schedule = {
         'task': 'apps.core.tasks.get_currency',
         'schedule': crontab(minute="*/3"),
     },
-    'specification_stop': {
-        'task': 'apps.specification.tasks.specification_date_stop',
-        'schedule': crontab(hour=0 , minute=15 ),
-    },
-    'add_iek': {
-        'task': 'apps.supplier.tasks.add_iek',
-        'schedule': crontab(hour=3,minute=0 ),
-    },
-    'add_veda': {
-        'task': 'apps.supplier.tasks.add_veda',
-        'schedule': crontab(hour=1, minute=0 ),
-    },
-    'add_prompower': {
-        'task': 'apps.supplier.tasks.add_prompower',
-        'schedule': crontab(hour=14, minute=0 ),
-    },
+    # 'specification_stop': {
+    #     'task': 'apps.specification.tasks.specification_date_stop',
+    #     'schedule': crontab(hour=0 , minute=15 ),
+    # },
+    # 'add_iek': {
+    #     'task': 'apps.supplier.tasks.add_iek',
+    #     'schedule': crontab(hour=3,minute=0 ),
+    # },
+    # 'add_veda': {
+    #     'task': 'apps.supplier.tasks.add_veda',
+    #     'schedule': crontab(hour=1, minute=0 ),
+    # },
+    # 'add_prompower': {
+    #     'task': 'apps.supplier.tasks.add_prompower',
+    #     'schedule': crontab(hour=14, minute=0 ),
+    # },
 }

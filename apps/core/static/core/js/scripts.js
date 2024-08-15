99ee4d132dc99b1499c912e7147c678743d23d40
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2024-08-15 08:44:12 +0400

    мердж еще

--- apps/core/static/core/js/scripts.js
+++ apps/core/static/core/js/scripts.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import "/static/core/js/slider.js";
 import "/static/core/js/nunjucks.min.js"
 import "/static/admin_specification/js/scripts.js";
-=======
-import "/static/core/js/slider.js";
-import "/static/core/js/scripts/web.js";
-import "/static/admin_specification/js/scripts.js";
->>>>>>> 56b375d773cb4595c724124c206df2a2dd5ba548

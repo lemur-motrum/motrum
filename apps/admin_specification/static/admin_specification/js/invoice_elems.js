1d50dc08ace762fd2009b660489b1aeb37512a50
Shvechkova <anastasiashvechkova@gmail.com>
2024-10-22 17:23:19 +0400

    1

--- apps/admin_specification/static/admin_specification/js/invoice_elems.js
+++ apps/admin_specification/static/admin_specification/js/invoice_elems.js
@@ -54,80 +54,81 @@ window.addEventListener("DOMContentLoaded", () => {
           );
           const specificationId =
             specificationItem.getAttribute("specification-id");
+          if (invoiceBtn) {
+            invoiceBtn.onclick = () => {
+              invoiceOverlay.classList.add("show");
+              document.body.style.overflowY = "hidden";
+              setTimeout(() => {
+                invoiceOverlay.classList.add("visible");
+              }, 600);
+              loadInvoiceItems(specificationId);
 
-          invoiceBtn.onclick = () => {
-            invoiceOverlay.classList.add("show");
-            document.body.style.overflowY = "hidden";
-            setTimeout(() => {
-              invoiceOverlay.classList.add("visible");
-            }, 600);
-            loadInvoiceItems(specificationId);
-
-            const interval = setInterval(() => {
-              const invoiceItems =
-                invoiceContainer.querySelectorAll(".invoice-item");
-              if (invoiceItems.length > 0) {
-                clearInterval(interval);
-                const createInvoiceBtn =
-                  modalWindow.querySelector(".invoice-btn");
-                createInvoiceBtn.onclick = () => {
-                  let validate = true;
-                  const inputs = modalWindow.querySelectorAll(
-                    ".invoice-data-input"
-                  );
-                  for (let i = 0; i < inputs.length; i += 1) {
-                    if (!inputs[i].value) {
-                      validate = false;
-                      break;
+              const interval = setInterval(() => {
+                const invoiceItems =
+                  invoiceContainer.querySelectorAll(".invoice-item");
+                if (invoiceItems.length > 0) {
+                  clearInterval(interval);
+                  const createInvoiceBtn =
+                    modalWindow.querySelector(".invoice-btn");
+                  createInvoiceBtn.onclick = () => {
+                    let validate = true;
+                    const inputs = modalWindow.querySelectorAll(
+                      ".invoice-data-input"
+                    );
+                    for (let i = 0; i < inputs.length; i += 1) {
+                      if (!inputs[i].value) {
+                        validate = false;
+                        break;
+                      }
                     }
-                  }
-                  if (validate == true) {
-                    const csrfToken = getCookie("csrftoken");
-                    const dataArray = [];
-                    invoiceItems.forEach((el) => {
-                      const id = el.getAttribute("invoice-elem-id");
-                      const value = el.querySelector(
-                        ".invoice-data-input"
-                      ).value;
+                    if (validate == true) {
+                      const csrfToken = getCookie("csrftoken");
+                      const dataArray = [];
+                      invoiceItems.forEach((el) => {
+                        const id = el.getAttribute("invoice-elem-id");
+                        const value = el.querySelector(
+                          ".invoice-data-input"
+                        ).value;
 
-                      const dataObject = {
-                        id: id,
-                        text_delivery: value,
-                      };
-                      dataArray.push(dataObject);
-                    });
+                        const dataObject = {
+                          id: id,
+                          text_delivery: value,
+                        };
+                        dataArray.push(dataObject);
+                      });
 
-                    const data = JSON.stringify(dataArray);
-                    console.log(data);
-                    fetch(
-                      `/api/v1/order/${specificationId}/create-bill-admin/`,
-                      {
-                        method: "UPDATE",
-                        headers: {
-                          "Content-Type": "application/json",
-                          "X-CSRFToken": csrfToken,
-                        },
-                        body: data,
-                      }
-                    ).then((response) => {
-                      if (response.status === 200) {
-                        invoiceOverlay.classList.remove("visible");
-                        if (invoiceOverlay.classList.contains("show")) {
-                          document.body.style.overflowY = "scroll";
+                      const data = JSON.stringify(dataArray);
+                      console.log(data);
+                      fetch(
+                        `/api/v1/order/${specificationId}/create-bill-admin/`,
+                        {
+                          method: "UPDATE",
+                          headers: {
+                            "Content-Type": "application/json",
+                            "X-CSRFToken": csrfToken,
+                          },
+                          body: data,
                         }
-                        setTimeout(() => {
-                          invoiceOverlay.classList.remove("show");
-                          invoiceContainer.innerHTML = "";
-                        }, 600);
-                      }
-                    });
-                  } else {
-                    console.log("Ошибка");
-                  }
-                };
-              }
-            });
-          };
+                      ).then((response) => {
+                        if (response.status === 200) {
+                          invoiceOverlay.classList.remove("visible");
+                          if (invoiceOverlay.classList.contains("show")) {
+                            document.body.style.overflowY = "scroll";
+                          }
+                          setTimeout(() => {
+                            invoiceOverlay.classList.remove("show");
+                            invoiceContainer.innerHTML = "";
+                          }, 600);
+                        }
+                      });
+                    } else {
+                      console.log("Ошибка");
+                    }
+                  };
+                }
+              });
+            };
+          }
           invoiceOverlay.onclick = () => {
             invoiceOverlay.classList.remove("visible");
             if (invoiceOverlay.classList.contains("show")) {

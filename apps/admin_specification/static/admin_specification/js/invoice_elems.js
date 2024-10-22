74bd025568a4edb5c81d5c37e28c000a719174f2
Shvechkova <anastasiashvechkova@gmail.com>
2024-10-22 13:13:15 +0000

    Создание счета

--- apps/admin_specification/static/admin_specification/js/invoice_elems.js
+++ apps/admin_specification/static/admin_specification/js/invoice_elems.js
@@ -1,42 +1,46 @@
 import { showErrorValidation, getCookie } from "/static/core/js/functions.js";
 
-const csrfToken = getCookie("csrftoken");
-
-function loadInvoiceItems() {
-  let params = new URLSearchParams(data);
-  let csrfToken = getCookie("csrftoken");
-  fetch(`/api/v1/order/load-ajax-specification-list/?${params.toString()}`, {
-    method: "GET",
-    headers: {
-      "X-CSRFToken": csrfToken,
-    },
-  })
-    .then((response) => response.json())
-    .then(function (data) {
-      console.log("data", data);
-      for (let i in data.data) {
-        console.log("data1", addAjaxCatalogItem(data.data[i]));
-      }
-    });
-}
-function renderCatalogItem(orderData) {
-  const ajaxTemplateWrapper = modalWindow.querySelector(".invoice_content");
-  const ajaxCatalogElementTemplate =
-    ajaxTemplateWrapper.querySelector("invoice-item").innerText;
-  return nunjucks.renderString(ajaxCatalogElementTemplate, orderData);
-}
-
-function addAjaxCatalogItem(ajaxElemData) {
-  let renderCatalogItemHtml = renderCatalogItem(ajaxElemData);
-  specificationContainer.insertAdjacentHTML("beforeend", renderCatalogItemHtml);
-}
-
 window.addEventListener("DOMContentLoaded", () => {
   const specificationContainer = document.querySelector(
     ".specification_container"
   );
   const invoiceOverlay = document.querySelector(".invoice-overlay");
   const modalWindow = invoiceOverlay.querySelector(".modal-window");
+  const invoiceContainer = modalWindow.querySelector(
+    '[invoice-elem="container"]'
+  );
+
+  function loadInvoiceItems(specificationId) {
+    let csrfToken = getCookie("csrftoken");
+    fetch(`/api/v1/order/${specificationId}/get-specification-product/`, {
+      method: "GET",
+      headers: {
+        "X-CSRFToken": csrfToken,
+      },
+    })
+      .then((response) => response.json())
+      .then(function (data) {
+        for (let i in data) {
+          addAjaxCatalogItem(data[i]);
+        }
+      });
+  }
+  function renderCatalogItem(orderData) {
+    let ajaxTemplateWrapper = document.querySelector(
+      '[template-elem="wrapper"]'
+    );
+    let ajaxCatalogElementTemplate = ajaxTemplateWrapper.querySelector(
+      '[invoice-elem="invoice-item"]'
+    ).innerText;
+
+    return nunjucks.renderString(ajaxCatalogElementTemplate, orderData);
+  }
+
+  function addAjaxCatalogItem(ajaxElemData) {
+    let renderCatalogItemHtml = renderCatalogItem(ajaxElemData);
+    invoiceContainer.insertAdjacentHTML("beforeend", renderCatalogItemHtml);
+  }
+
   if (specificationContainer) {
     const interval = setInterval(() => {
       const specificationsItems = specificationContainer.querySelectorAll(
@@ -48,6 +52,8 @@ window.addEventListener("DOMContentLoaded", () => {
           const invoiceBtn = specificationItem.querySelector(
             ".create-bill-button"
           );
+          const specificationId =
+            specificationItem.getAttribute("specification-id");
 
           invoiceBtn.onclick = () => {
             invoiceOverlay.classList.add("show");
@@ -55,7 +61,72 @@ window.addEventListener("DOMContentLoaded", () => {
             setTimeout(() => {
               invoiceOverlay.classList.add("visible");
             }, 600);
-            loadInvoiceItems();
+            loadInvoiceItems(specificationId);
+
+            const interval = setInterval(() => {
+              const invoiceItems =
+                invoiceContainer.querySelectorAll(".invoice-item");
+              if (invoiceItems.length > 0) {
+                clearInterval(interval);
+                const createInvoiceBtn =
+                  modalWindow.querySelector(".invoice-btn");
+                createInvoiceBtn.onclick = () => {
+                  let validate = true;
+                  const inputs = modalWindow.querySelectorAll(
+                    ".invoice-data-input"
+                  );
+                  for (let i = 0; i < inputs.length; i += 1) {
+                    if (!inputs[i].value) {
+                      validate = false;
+                      break;
+                    }
+                  }
+                  if (validate == true) {
+                    const csrfToken = getCookie("csrftoken");
+                    const dataArray = [];
+                    invoiceItems.forEach((el) => {
+                      const id = el.getAttribute("invoice-elem-id");
+                      const value = el.querySelector(
+                        ".invoice-data-input"
+                      ).value;
+
+                      const dataObject = {
+                        id: id,
+                        text_delivery: value,
+                      };
+                      dataArray.push(dataObject);
+                    });
+
+                    const data = JSON.stringify(dataArray);
+                    console.log(data);
+                    fetch(
+                      `/api/v1/order/${specificationId}/create-bill-admin/`,
+                      {
+                        method: "UPDATE",
+                        headers: {
+                          "Content-Type": "application/json",
+                          "X-CSRFToken": csrfToken,
+                        },
+                        body: data,
+                      }
+                    ).then((response) => {
+                      if (response.status === 200) {
+                        invoiceOverlay.classList.remove("visible");
+                        if (invoiceOverlay.classList.contains("show")) {
+                          document.body.style.overflowY = "scroll";
+                        }
+                        setTimeout(() => {
+                          invoiceOverlay.classList.remove("show");
+                          invoiceContainer.innerHTML = "";
+                        }, 600);
+                      }
+                    });
+                  } else {
+                    console.log("Ошибка");
+                  }
+                };
+              }
+            });
           };
           invoiceOverlay.onclick = () => {
             invoiceOverlay.classList.remove("visible");
@@ -64,6 +135,7 @@ window.addEventListener("DOMContentLoaded", () => {
             }
             setTimeout(() => {
               invoiceOverlay.classList.remove("show");
+              invoiceContainer.innerHTML = "";
             }, 600);
           };
           modalWindow.onclick = (e) => {
--- apps/core/templatetags/js_templates.py
+++ apps/core/templatetags/js_templates.py
@@ -4,43 +4,45 @@ from django.template import loader
 register = template.Library()
 
 
-@register.inclusion_tag('core/includes/js_templates.html', takes_context=False)
+@register.inclusion_tag("core/includes/js_templates.html", takes_context=False)
 def js_templates():
     template_list = [
         {
-            'module': 'catalog',
-            'name': 'product-item',
-            'template': 'product/includes/ajax_product_item.html',
+            "module": "catalog",
+            "name": "product-item",
+            "template": "product/includes/ajax_product_item.html",
         },
         {
-            'module': 'lk',
-            'name': 'order-item',
-            'template': 'client/includes/ajax_order_item.html',
-        }
-        ,
+            "module": "lk",
+            "name": "order-item",
+            "template": "client/includes/ajax_order_item.html",
+        },
         {
-            'module': 'lk',
-            'name': 'document-item',
-            'template': 'client/includes/ajax_document_item.html',
-        }
-        ,
+            "module": "lk",
+            "name": "document-item",
+            "template": "client/includes/ajax_document_item.html",
+        },
         {
-            'module': 'project',
-            'name': 'project-item',
-            'template': 'projects_web/includes/ajax_project_item.html',
-        }
-                ,
+            "module": "project",
+            "name": "project-item",
+            "template": "projects_web/includes/ajax_project_item.html",
+        },
         {
-            'module': 'specification',
-            'name': 'specification-item',
-            'template': 'admin_specification/include/ajax_specification_item.html',
-        }
+            "module": "specification",
+            "name": "specification-item",
+            "template": "admin_specification/include/ajax_specification_item.html",
+        },
+        {
+            "module": "invoice",
+            "name": "invoice-item",
+            "template": "admin_specification/include/ajax_invoice_item.html",
+        },
     ]
 
     for template_item in template_list:
-        loaded_template = loader.get_template(template_item['template'])
-        template_item['template'] = loaded_template.template.source
+        loaded_template = loader.get_template(template_item["template"])
+        template_item["template"] = loaded_template.template.source
 
     return {
-        'template_list': template_list,
+        "template_list": template_list,
     }

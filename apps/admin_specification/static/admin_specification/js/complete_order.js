56bd25d413294722c93108baa621226c0da29342
Shvechkova <anastasiashvechkova@gmail.com>
2024-10-28 13:28:37 +0400

    1

--- apps/admin_specification/static/admin_specification/js/complete_order.js
+++ apps/admin_specification/static/admin_specification/js/complete_order.js
@@ -18,55 +18,57 @@ export function completeOrder(container) {
             ".complete_order_button"
           );
           const orderId = specification.getAttribute("order-id");
-          completeBtn.onclick = () => {
-            overlay.classList.add("show");
-            modalWindow.setAttribute("order-id", orderId);
-            document.body.style.overflowY = "hidden";
-            setTimeout(() => {
-              overlay.classList.add("visible");
-            }, 600);
-          };
-          addOrderButton.onclick = () => {
-            if (!calendar.value) {
-              showErrorValidation("Поле календаря не заполнено", error);
-            }
-            if (calendar.value) {
-              addOrderButton.disabled = true;
-              addOrderButton.textContent = "";
-              addOrderButton.innerHTML = "<div class='small_loader'></div>";
+          if (completeBtn) {
+            completeBtn.onclick = () => {
+              overlay.classList.add("show");
+              modalWindow.setAttribute("order-id", orderId);
+              document.body.style.overflowY = "hidden";
+              setTimeout(() => {
+                overlay.classList.add("visible");
+              }, 600);
+            };
+            addOrderButton.onclick = () => {
+              if (!calendar.value) {
+                showErrorValidation("Поле календаря не заполнено", error);
+              }
+              if (calendar.value) {
+                addOrderButton.disabled = true;
+                addOrderButton.textContent = "";
+                addOrderButton.innerHTML = "<div class='small_loader'></div>";
 
-              const objData = {
-                date_completed: calendar.value,
-              };
-              const data = JSON.stringify(objData);
-              const idOrder = modalWindow.getAttribute("order-id");
-              fetch(`/api/v1/order/${idOrder}/add-date-completed/`, {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  "X-CSRFToken": csrfToken,
-                },
-                body: data,
-              }).then((response) => {
-                if (response.status == 200) {
-                  overlay.classList.remove("visible");
-                  if (overlay.classList.contains("show")) {
-                    document.body.style.overflowY = "scroll";
+                const objData = {
+                  date_completed: calendar.value,
+                };
+                const data = JSON.stringify(objData);
+                const idOrder = modalWindow.getAttribute("order-id");
+                fetch(`/api/v1/order/${idOrder}/add-date-completed/`, {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": csrfToken,
+                  },
+                  body: data,
+                }).then((response) => {
+                  if (response.status == 200) {
+                    overlay.classList.remove("visible");
+                    if (overlay.classList.contains("show")) {
+                      document.body.style.overflowY = "scroll";
+                    }
+                    modalWindow.setAttribute("order-id", "");
+                    setTimeout(() => {
+                      overlay.classList.remove("show");
+                      const currentDate = new Date().toISOString().slice(0, 10);
+                      calendar.value = currentDate;
+                    }, 600);
+                    console.log("ок");
+                    addOrderButton.disabled = false;
+                    addOrderButton.innerHTML = "";
+                    addOrderButton.textContent = "Завершить заказ";
                   }
-                  modalWindow.setAttribute("order-id", "");
-                  setTimeout(() => {
-                    overlay.classList.remove("show");
-                    const currentDate = new Date().toISOString().slice(0, 10);
-                    calendar.value = currentDate;
-                  }, 600);
-                  console.log("ок");
-                  addOrderButton.disabled = false;
-                  addOrderButton.innerHTML = "";
-                  addOrderButton.textContent = "Завершить заказ";
-                }
-              });
-            }
-          };
+                });
+              }
+            };
+          }
         });
       }
     });

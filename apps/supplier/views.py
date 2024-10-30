717b71eb1d3e5ce3a730f81d9632f44cb5526f65
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2024-10-30 11:18:47 +0400

    1

--- apps/supplier/views.py
+++ apps/supplier/views.py
@@ -34,7 +34,7 @@ from simple_history.utils import update_change_reason
 def add_iek(request):
 
     title = "Услуги"
-    get_motrum_storage()
+    iek_api()
     
     responsets = ["233", "2131"]
     # responsets = 0

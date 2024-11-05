6b7d103a12d7d15ed7dec4634d17de6d2ac4410b
Shvechkova <anastasiashvechkova@gmail.com>
2024-11-05 16:25:36 +0400

    1

--- apps/admin_specification/static/admin_specification/js/edit_motrum_price.js
+++ apps/admin_specification/static/admin_specification/js/edit_motrum_price.js
@@ -4,10 +4,17 @@ export function editMotrumPrice(container) {
   if (container) {
     const specificationItems = container.querySelectorAll(".item_container");
     specificationItems.forEach((specification) => {
+      const priceInput = specification.querySelector(".price-input");
       const motrumPriceContainer = specification.querySelector(".price_motrum");
+      if (priceInput) {
+        priceInput.oninput = () => {
+          motrumPriceContainer.setAttribute("price-motrum", priceInput.value);
+        };
+      }
       const motrumPriceOne = getCurrentPrice(
         motrumPriceContainer.getAttribute("price-motrum")
       );
+
       const quantityItem = specification.querySelector(".input-quantity").value;
       const currentMotrumPrice = +motrumPriceOne * +quantityItem;
       console.log("currentMotrumPrice", currentMotrumPrice);

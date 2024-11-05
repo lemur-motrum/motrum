e0695f386c6ce10f3c94e2e0530752b199d43611
Shvechkova <anastasiashvechkova@gmail.com>
2024-11-05 14:22:30 +0400

    форматирование недель в счете

--- apps/admin_specification/static/admin_specification/js/create_invoice_without_specification.js
+++ apps/admin_specification/static/admin_specification/js/create_invoice_without_specification.js
@@ -162,9 +162,32 @@ window.addEventListener("DOMContentLoaded", () => {
               })
               .then((response2) => {
                 const dataObj = response2.map((elem) => {
+                  const createTextDateDelivery = () => {
+                    const orderData = new Date(elem["date_delivery"]);
+                    const today = new Date();
+                    const delta = orderData.getTime() - today.getTime();
+                    const dayDifference = +Math.floor(
+                      delta / 1000 / 60 / 60 / 24
+                    );
+                    const resultDays = +Math.ceil(dayDifference / 7);
+
+                    function num_word(value, words) {
+                      value = Math.abs(value) % 100;
+                      var num = value % 10;
+                      if (value > 10 && value < 20) return words[2];
+                      if (num > 1 && num < 5) return words[1];
+                      if (num == 1) return words[0];
+                      return words[2];
+                    }
+                    return `${resultDays} ${num_word(resultDays, [
+                      "неделя",
+                      "недели",
+                      "недель",
+                    ])}`;
+                  };
                   return {
                     id: elem["id"],
-                    text_delivery: elem["date_delivery"],
+                    text_delivery: createTextDateDelivery(),
                   };
                 });
                 const data = JSON.stringify(dataObj);

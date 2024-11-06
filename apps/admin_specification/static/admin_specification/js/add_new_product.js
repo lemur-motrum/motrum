23fefc86a4dbe48ba8fe6a2f6d56bb4ca5939db6
Shvechkova <anastasiashvechkova@gmail.com>
2024-11-06 11:24:44 +0400

    1

--- apps/admin_specification/static/admin_specification/js/add_new_product.js
+++ apps/admin_specification/static/admin_specification/js/add_new_product.js
@@ -4,82 +4,33 @@ import {
   inputValidationQuantity,
 } from "../js/add_new_product_without_cart.js";
 
+const csrfToken = getCookie("csrftoken");
+
 window.addEventListener("DOMContentLoaded", () => {
-  const specificationTable = document.querySelector(".spetification_table");
-  if (specificationTable) {
-    const csrfToken = getCookie("csrftoken");
-    const addNewProductBtn =
-      specificationTable.querySelector(".add_new_product");
-    const overlay = document.querySelector(
-      ".overlay_new_product_in_specification"
+  const globalCartWrapper = document.querySelector(".spetification_table");
+  if (globalCartWrapper) {
+    const addNewProductContainer = globalCartWrapper.querySelector(
+      ".add_new_product_container"
     );
-    const modalWindow = overlay.querySelector(".modal_window");
-    const form = modalWindow.querySelector("form");
-    const nameInput = modalWindow.querySelector(".name");
-    const nameInputError = modalWindow.querySelector(".name-error");
-    const articleInput = modalWindow.querySelector(".article");
-    const articleInputError = modalWindow.querySelector(".article-error");
-    const priceInput = modalWindow.querySelector(".price");
-    const priceInputError = modalWindow.querySelector(".price-error");
-    const quantityInput = modalWindow.querySelector(".quantity");
-    const quantityInputError = modalWindow.querySelector(".quantity-error");
-    const closeBtn = modalWindow.querySelector(".close-btn");
+    const searchInput = addNewProductContainer.querySelector(".search_input");
+    const searchElemsContainer =
+      addNewProductContainer.querySelector(".search_container");
+    const addProductButton =
+      addNewProductContainer.querySelector(".add_product");
+    const addNewProductButton =
+      addNewProductContainer.querySelector(".add_new_product");
 
-    addNewProductBtn.onclick = () => {
-      overlay.classList.add("show");
-      inputValidation(priceInput);
-      inputValidationQuantity(quantityInput);
-      if (overlay.classList.contains("show")) {
-        document.body.style.overflow = "hidden";
-      }
-      setTimeout(() => {
-        overlay.classList.add("visible");
-      });
-
-      closeBtn.onclick = () => {
-        overlay.classList.remove("visible");
-        if (overlay.classList.contains("show")) {
-          document.body.style.overflowY = "scroll";
-        }
-        setTimeout(() => {
-          overlay.classList.remove("show");
-        }, 600);
-        form.reset();
-      };
-    };
-    modalWindow.onclick = (e) => {
-      e.stopPropagation();
-    };
-
-    form.onsubmit = (e) => {
-      e.preventDefault();
-      if (!nameInput.value) {
-        showErrorValidation("Обязательное поле", nameInputError);
-      }
-      if (!priceInput.value) {
-        showErrorValidation("Обязательное поле", priceInputError);
-      }
-      if (!quantityInput.value) {
-        showErrorValidation("Обязательное поле", quantityInputError);
-      }
-      if (!articleInput.value) {
-        showErrorValidation("Обязательное поле", articleInputError);
-      }
-      if (nameInput.value && priceInput.value && quantityInput.value) {
-        console.log("Ура, товар добавлен в корзину");
-        const cart_id = getCookie("cart");
-        const dataObj = {
-          product: null,
-          product_new: nameInput.value,
-          product_new_article: articleInput.value,
-          product_new_price: +priceInput.value,
-          cart: +cart_id,
-          quantity: +quantityInput.value,
+    searchInput.oninput = () => {
+      if (searchInput.value.length >= 3) {
+        searchElemsContainer.classList.add("show");
+        searchElemsContainer.innerHTML = "<div class='small_loader'></div>";
+        const objData = {
+          search_text: searchInput.value,
+          count: 0,
+          count_last: 9,
         };
-
-        const data = JSON.stringify(dataObj);
-
-        fetch(`/api/v1/cart/${cart_id}/save-product-new/`, {
+        const data = JSON.stringify(objData);
+        fetch("/api/v1/product/search-product/", {
           method: "POST",
           body: data,
           headers: {
@@ -88,26 +39,21 @@ window.addEventListener("DOMContentLoaded", () => {
           },
         })
           .then((response) => {
-            if (response.status == 200) {
-              location.reload();
+            if (response.status === 200) {
               return response.json();
-            }
-            if (response.status == 409) {
-              showErrorValidation(
-                "Товар с таким артикулом в корзине уже есть ",
-                articleInputError
-              );
             } else {
               throw new Error("Ошибка");
             }
           })
-          .then(
-            (response) =>
-              (document.querySelector(
-                ".admin_specification_cart_length"
-              ).textContent = response.cart_len)
-          )
-          .catch((error) => console.error(error));
+          .then((response) => {
+            searchElemsContainer.innerHTML = "";
+            response.data.forEach((el) => {
+              searchElemsContainer.innerHTML += `<div class="product_search_item">${el.name}</div>`;
+            });
+          });
+      } else {
+        searchElemsContainer.innerHTML = "";
+        searchElemsContainer.classList.remove("show");
       }
     };
   }

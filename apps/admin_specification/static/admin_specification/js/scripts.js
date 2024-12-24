e35c701dae8e37377874697eac6f6faa0340fd55
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2024-12-24 09:50:42 +0400

    111

--- apps/admin_specification/static/admin_specification/js/scripts.js
+++ apps/admin_specification/static/admin_specification/js/scripts.js
@@ -1565,7 +1565,7 @@ window.addEventListener("DOMContentLoaded", () => {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          document.cookie = `specificationId=${data["specification"]}; path=/; SameSite=None; Secure`;
+          document.cookie = `specificationId=${specificationId}; path=/; SameSite=None; Secure`;
           document.location.href = "/admin_specification/current_specification/";
         });
     };
@@ -1582,7 +1582,7 @@ window.addEventListener("DOMContentLoaded", () => {
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
-        document.cookie = `specificationId=${data["specification"]}; path=/; SameSite=None; Secure`;
+        document.cookie = `specificationId=${specificationId}; path=/; SameSite=None; Secure`;
         document.location.href = "/admin_specification/current_specification/";
       });
     };

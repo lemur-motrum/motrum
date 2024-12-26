ec29fabf176c7a6f08f3bdbc2f1a44991c90f089
Shvechkova <anastasiashvechkova@gmail.com>
2024-12-26 14:44:07 +0400

    1

--- apps/admin_specification/static/admin_specification/js/reload_bitrix_page.js
+++ apps/admin_specification/static/admin_specification/js/reload_bitrix_page.js
@@ -1,8 +1,8 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const wrapper = document.querySelector(".bitrix_product_container");
-  if (wrapper) {
-    setTimeout(() => {
-      window.location.reload();
-    }, 60000);
-  }
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//   const wrapper = document.querySelector(".bitrix_product_container");
+//   if (wrapper) {
+//     setTimeout(() => {
+//       window.location.reload();
+//     }, 60000);
+//   }
+// });

0fb44ebc582aceb2f7ae4d0e25a818bb67c67428
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2025-02-06 09:07:40 +0400

    1

--- project/celery.py
+++ project/celery.py
@@ -15,7 +15,7 @@ app.conf.beat_schedule = {
 
     "add_iek": {
         "task": "apps.supplier.tasks.add_iek",
-        "schedule": crontab(minute=00, hour=14),
+        "schedule": crontab(minute=30, hour=9),
     },
 
     # # ТАСКИ битрикс

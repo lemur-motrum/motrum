79f4ab3d10b42e84ce265c2351ca156bf36ad1dc
Shvechkova <anastasiashvechkova@gmail.com>
2025-03-27 10:35:22 +0400

    12

--- apps/core/static/core/js/scripts/manager-popup-logic.js
+++ apps/core/static/core/js/scripts/manager-popup-logic.js
@@ -1,8 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
   const managerPopUp = document.querySelector(".personal-manager-container");
   if (managerPopUp) {
+    const overlay = managerPopUp.querySelector(".manager_callback_overlay");
+    const openOverlayBtn = managerPopUp.querySelector(".open_overlay_btn");
+    const closeOverlayBtn = overlay.querySelector(".close_btn");
+
     setTimeout(() => {
       managerPopUp.classList.add("visible");
     }, 3000);
+
+    function openOverlay() {
+      overlay.classList.add("show");
+      setTimeout(() => {
+        overlay.classList.add("visible");
+      });
+    }
+
+    function closeOverlay() {
+      overlay.classList.remove("visible");
+      setTimeout(() => {
+        overlay.classList.remove("show");
+      }, 600);
+    }
+
+    openOverlayBtn.onclick = () => openOverlay();
+    closeOverlayBtn.onclick = () => closeOverlay();
   }
 });

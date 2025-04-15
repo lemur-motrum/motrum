fd6dc0bb78fbf0d5ff6e06a3ee7cf6045b9b2fdf
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2025-04-15 10:17:54 +0400

    Разные цвета фона для теста и продакшена

--- apps/core/templatetags/test_display.py
+++ apps/core/templatetags/test_display.py
@@ -1,6 +1,6 @@
 from django import template
 from apps.user.models import AdminUser
-from project.settings import IS_TESTING, IS_WEB
+from project.settings import IS_PROD, IS_TESTING, IS_WEB
 
 register = template.Library()
 
@@ -8,9 +8,12 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def test_display(context):
     pass
-    # if IS_TESTING:
-    #     pass
-    # else:
+    if IS_TESTING:
+        text = 'style=background-color:bisque;'
+        return text
+    else:
+        pass
+        
     #     request = context["request"]
 
     #     if request.user.id:

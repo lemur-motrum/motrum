93e719d6332975213e0e6ab23db7484ecfc5eaff
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2025-06-03 09:51:00 +0400

    Добавлено сохранение комментария из product_item в функции save_specification в файле apps/core/utils.py

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -1147,7 +1147,11 @@ def save_specification(
             text_delivery = product_item["text_delivery"]
             if text_delivery != "" and text_delivery != None:
                 product_spes.text_delivery = text_delivery
-
+                
+            item_comm = product_item["comment"]
+            if item_comm != "" and item_comm != None:
+                product_spes.text_delivery = item_comm
+                
             product_spes.save()
 
             total_amount = total_amount + price_all
@@ -1236,6 +1240,10 @@ def save_specification(
             text_delivery = product_item["text_delivery"]
             if text_delivery != "" and text_delivery != None:
                 product_spes.text_delivery = text_delivery
+            
+            item_comm = product_item["comment"]
+            if item_comm != "" and item_comm != None:
+                product_spes.text_delivery = item_comm
 
             product_spes.save()
 

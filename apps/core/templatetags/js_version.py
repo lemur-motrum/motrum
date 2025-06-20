ac70cbba4924d04f68e7deb3664b0eb0f7776ee1
Trilshane <Trilshane@yandex.ru>
2025-06-20 14:50:23 +0400

    Попробовал увеличить загрузку сайта

--- apps/core/templatetags/js_version.py
+++ apps/core/templatetags/js_version.py
@@ -1,9 +1,15 @@
 from django import template
+from project.settings import IS_TESTING
+import random
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=False)
 def js_version():
-    ver = "1.1"
+    if IS_TESTING:
+        ver = random.randint(0,3124567)
+    else:
+        ver = "1.1"
+
     return ver

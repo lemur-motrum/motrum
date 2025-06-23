ee6631f115e3ae56fb60c6c7f7dde8402c02fcb3
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2025-06-23 15:00:02 +0400

    Обновлена функция js_version в файле apps/core/templatetags/js_version.py: заменено случайное значение версии на фиксированное "1.1" в режиме тестирования.

--- apps/core/templatetags/js_version.py
+++ apps/core/templatetags/js_version.py
@@ -8,7 +8,8 @@ register = template.Library()
 @register.simple_tag(takes_context=False)
 def js_version():
     if IS_TESTING:
-        ver = random.randint(0,3124567)
+        # ver = random.randint(0,3124567)
+        ver = "1.1"
     else:
         ver = "1.1"
 

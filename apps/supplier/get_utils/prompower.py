9430ea1668c7b3c5bfcb10a2b03fecd7605073d8
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2025-07-23 13:51:39 +0400

    Изменение условия проверки статьи в функции prompower_api в файле apps/supplier/get_utils/prompower.py: обновлено сравнение с новым значением "PD310-A420K".

--- apps/supplier/get_utils/prompower.py
+++ apps/supplier/get_utils/prompower.py
@@ -206,7 +206,7 @@ def prompower_api():
 
             try:
                 i += 1
-                if data_item["article"] != None and data_item["article"] == "PD310A4100K":
+                if data_item["article"] != None and data_item["article"] == "PD310-A420K":
                     print("!!!!!!!!!!!!!!!!number", i)
                     # основная инфа
                     article_suppliers = data_item["article"]

e97739461c54d23291aeac1dc12f13b74e3bdf10
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2025-08-14 10:01:34 +0400

    Закомментированы строки с отправкой данных в функцию OrderViewSet в файле apps/client/api/view_sets.py для предотвращения выполнения запроса к API 1C.

--- apps/client/api/view_sets.py
+++ apps/client/api/view_sets.py
@@ -1164,10 +1164,10 @@ class OrderViewSet(viewsets.ModelViewSet):
 
                 type_save = request.COOKIES.get("type_save")
                 
-                json_data = json.dumps(data_for_1c)
-                url = "https://dev.bmgspb.ru/grigorev_unf_m/hs/rest/order"
-                headers = {"Content-type": "application/json"}
-                response = send_requests(url, headers, json_data, "1c")
+                # json_data = json.dumps(data_for_1c)
+                # url = "https://dev.bmgspb.ru/grigorev_unf_m/hs/rest/order"
+                # headers = {"Content-type": "application/json"}
+                # response = send_requests(url, headers, json_data, "1c")
                 
                 if IS_TESTING or user.username == "testadmin":
                     pass
